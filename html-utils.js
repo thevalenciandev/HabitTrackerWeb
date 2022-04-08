0a743417ce6a3ffe1388a34281509f2e1acf1fdb
thevalenciandev <thevalenciandev@gmail.com>
2022-04-08 11:37:15 +0100

    Refactor: keep logic and html scaffolding separate. Remove edit

--- html-utils.js
+++ html-utils.js
@@ -4,7 +4,7 @@ function displayOutcome(divId, outcome) {
   setTimeout(() => (elm.innerHTML = ""), 3000); //Remove after 3 seconds
 }
 
-function createListElement(e, elmType, getListElmFunction, addElmFunction, removeElmFunction) {
+function createListElement(e, onClickRemoveFunction) {
   // Create wrapping div for the element
   const elmDiv = document.createElement("div");
   elmDiv.id = e;
@@ -14,32 +14,11 @@ function createListElement(e, elmType, getListElmFunction, addElmFunction, remov
   lbl.innerText = e;
   elmDiv.appendChild(lbl);
 
-  // A button to allow editing
-  const btnEdit = document.createElement("button");
-  btnEdit.setAttribute("type", "button");
-  btnEdit.innerText = "Edit";
-  btnEdit.addEventListener("click", () => {
-    removeElmFunction(e);
-    const newE = document.getElementById("l" + elmType).value();
-    addElmFunction(newE);
-    displayOutcome("outcomeAction-" + elmType, "Sucessfully edited! 🎉");
-  });
-  elmDiv.appendChild(btnEdit);
-
   // And a button to allow removing
   const btnAdd = document.createElement("button");
   btnAdd.setAttribute("type", "button");
   btnAdd.innerText = "Remove";
-  btnAdd.addEventListener("click", () => {
-    removeElmFunction(e);
-    displayOutcome("outcomeAction-" + elmType, "Sucessfully removed! 🎉");
-    populateList(
-      elmType,
-      getListElmFunction,
-      addElmFunction,
-      removeElmFunction
-    ); // Refresh list again
-  });
+  btnAdd.addEventListener("click", onClickRemoveFunction);
 
   elmDiv.appendChild(btnAdd);
   return elmDiv;
